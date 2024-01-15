0a09106bc8d43ee7948b1f6d329011eed4895d09
aryanando <aryanando232@gmail.com>
2024-01-15 14:54:13 +0700

    Add Pre About and RIG

--- resources/views/dashboard/partials/rig.blade.php
+++ resources/views/dashboard/partials/rig.blade.php
@@ -1,10 +1,30 @@
 <section class="flex items-center overflow-hidden" id="rig-section">
     <div class="h-screen w-full" id="start-section">
         <div class="block h-full w-full bg-slate-900">
-            <div class="flex items-center h-full w-full justify-center">
 
+            <div class="flex items-center h-full w-full justify-center ">
+                <div>
+                    <h1 class=" text-white text-xl font-mono">Senjata Pamungkas Dalam Berkarya ...</h1>
+                    <div class="grid grid-cols-2 border border-5">
+
+                        <div class="p-10 ">
+                            <h2 class="text-white font-mono">Personal Computer</h2>
+                            <p class="text-white font-mono">Motherboard - Asus P8Z77v-Pro</p>
+                            <p class="text-white font-mono">Processor - Core i7-3770k</p>
+                            <p class="text-white font-mono">RAM - 16GB Team Dark 2400mhz</p>
+                            <p class="text-white font-mono">VGA - MSI RTX3060TI Gaming X Trio</p>
+                            <p class="text-white font-mono">OS - Windows 10 Enterprise LTSC</p>
+                        </div>
+                        <div class="p-10 ">
+                            <h2 class="text-white font-mono">NoteBook</h2>
+                            <p class="text-white font-mono">Macbook Pro - 13" 2019</p>
+                            <p class="text-white font-mono">Processor - Core i5-8257U</p>
+                            <p class="text-white font-mono">RAM - 8GB LPDDR3 2133mhz</p>
+                            <p class="text-white font-mono">OS - MacOs 14 Sonoma</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
     </div>
 </section>
