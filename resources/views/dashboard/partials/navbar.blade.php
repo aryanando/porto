8aae1eff8cf4bb2cd868476dd4edd43ee71979fb
Aryanando <aryanando@mb>
2024-01-15 13:32:38 +0700

    Fixing gihub link

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -39,7 +39,7 @@
                 </a>
             </div>
             <div>
-                <a href="#"
+                <a href="https://github.com/aryanando/" target="blank"
                     class=" border-2 font-extrabold inline-block text-sm px-4 py-2 leading-none rounded text-black border-black hover:border-transparent transition-all duration-500 hover:text-gray-400 hover:border-gray-400 mt-4 lg:mt-0 hover:transition-all hover:duration-500">GITHUB</a>
             </div>
         </div>
